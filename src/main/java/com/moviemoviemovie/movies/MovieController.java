3235135805e0df09feb635267e833f41965aa4b0
Luis <luisantoniodiaz97@gmail.com>
2024-04-19 15:06:26 -0600

    Update: Backend Terminado

--- src/main/java/com/moviemoviemovie/movies/MovieController.java
+++ src/main/java/com/moviemoviemovie/movies/MovieController.java
@@ -1,16 +1,30 @@
 package com.moviemoviemovie.movies;
 
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies() {
-        return new ResponseEntity<String>("All movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.getAllMovies(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{imdbId}")
+    public ResponseEntity<Optional<Movie>> getOneMovie(@PathVariable String imdbId){
+        return new ResponseEntity<Optional<Movie>>(movieService.getOneMovie(imdbId), HttpStatus.OK);
     }
 }
